f606a587f09a1909d5375512122b82fbdaf7ea9a
kannanmohan <kannan.c.mohan@mindcurv.com>
2022-04-30 19:20:55 +0200

    feat: updated controller

--- src/main/java/com/kcm/msp/dev/app/development/microservice/blueprint/AppDevelopmentMicroserviceBlueprintApplication.java
+++ src/main/java/com/kcm/msp/dev/app/development/microservice/blueprint/AppDevelopmentMicroserviceBlueprintApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppDevelopmentMicroserviceBlueprintApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppDevelopmentMicroserviceBlueprintApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AppDevelopmentMicroserviceBlueprintApplication.class, args);
+  }
 }
--- src/main/java/com/kcm/msp/dev/app/development/microservice/blueprint/controller/BlueprintController.java
+++ src/main/java/com/kcm/msp/dev/app/development/microservice/blueprint/controller/BlueprintController.java
@@ -1,11 +1,24 @@
 package com.kcm.msp.dev.app.development.microservice.blueprint.controller;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import com.kcm.msp.dev.app.development.microservice.blueprint.api.BlueprintApi;
+import com.kcm.msp.dev.app.development.microservice.blueprint.models.Pet;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class BlueprintController implements BlueprintApi {
 
+  @Override
+  public ResponseEntity<List<Pet>> listPets(Integer limit) {
+    final Pet pet = new Pet();
+    pet.setId(123L);
+    pet.setName("petName");
+    pet.setTag("petTag");
+    return ResponseEntity.status(OK).body(List.of(pet));
+  }
 }
