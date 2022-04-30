963f776455d98004f3aad45352ab317d1b347afb
kannanmohan <kannan.c.mohan@mindcurv.com>
2022-04-30 13:18:01 +0200

    initial changes

---
+++ src/main/java/com/kcm/msp/dev/appdevelopmentmicroserviceblueprint/AppDevelopmentMicroserviceBlueprintApplication.java
@@ -0,0 +1,13 @@
+package com.kcm.msp.dev.appdevelopmentmicroserviceblueprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDevelopmentMicroserviceBlueprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDevelopmentMicroserviceBlueprintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcm/msp/dev/appdevelopmentmicroserviceblueprint/AppDevelopmentMicroserviceBlueprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcm.msp.dev.appdevelopmentmicroserviceblueprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDevelopmentMicroserviceBlueprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
