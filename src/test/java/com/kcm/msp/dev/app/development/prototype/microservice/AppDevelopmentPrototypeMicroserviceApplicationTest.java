66c65b7e0323923158443904f1ba92cfb3c7722c
kannanmohan <kannan.c.mohan@mindcurv.com>
2022-05-27 09:13:43 +0200

    disable integration test with environment property

--- src/test/java/com/kcm/msp/dev/app/development/prototype/microservice/AppDevelopmentPrototypeMicroserviceApplicationTest.java
+++ src/test/java/com/kcm/msp/dev/app/development/prototype/microservice/AppDevelopmentPrototypeMicroserviceApplicationTest.java
@@ -1,11 +1,21 @@
 package com.kcm.msp.dev.app.development.prototype.microservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.kcm.msp.dev.app.development.prototype.microservice.controller.PrototypeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.DisabledIf;
+
+@DisabledIf(expression = "#{environment['skip.integration.test'] == 'true'}")
 @SpringBootTest
 class AppDevelopmentPrototypeMicroserviceApplicationTest {
 
+  @Autowired PrototypeController prototypeController;
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertNotNull(prototypeController, () -> "The prototypeController should not be null");
+  }
 }
